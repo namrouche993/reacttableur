f2edfdedacef330727a9be52a57fd70fc3da331f
namrouche993 <namrouche993@gmail.com>
2023-09-16 23:59:33 +0100

    add something to App.js fetch post

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import './App.css';
 import Navbar from './Navbar/Navbar';
 import Hottable from './Hottable';
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 import { last_row_after_header } from './initials_inputs.js';
 
 
 function App() {
+
+  useEffect(() => {
+    // Make a POST request to collect user data when the component mounts
+    fetch('http://localhost:5000/api/collectuserdata', {
+      method: 'POST',
+    })
+      .then((response) => {
+        if (response.ok) {
+          console.log('User data collected.');
+        } else {
+          console.error('Failed to collect user data.');
+        }
+      })
+      .catch((error) => {
+        console.error('Error collecting user data:', error);
+      });
+  }, []);
+
 
   return (
     <>
